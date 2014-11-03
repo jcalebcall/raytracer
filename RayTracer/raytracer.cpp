a7054740565a53b4b38f4cb2a9aa1413be9337e5
Caleb Call <jcalebcall@gmail.com>
2014-11-02 20:32:19 -0700

    Initial Triangles

--- RayTracer/raytracer.cpp
+++ RayTracer/raytracer.cpp
@@ -106,7 +106,7 @@ void parseFile(const std::string filename, CameraAndColorInfo &cameraAndColorInf
 }
 
 template<typename T>
-void computeRayDir(const int i, const int j, const int imageHeight, const int imageWidth,
+void computeRayDir(const float i, const float j, const int imageHeight, const int imageWidth,
 					 const float viewWindowValue, Vec3<T> &rayDir)
 {
 	float u = (i - 0) * ( (viewWindowValue - (-viewWindowValue)) / ( imageWidth - 0) ) + (-viewWindowValue);
@@ -117,7 +117,7 @@ void computeRayDir(const int i, const int j, const int imageHeight, const int im
 	rayDir.z = 0;
 
 	// s - eye
-	rayDir = rayDir - Vec3(0, 0, 1);
+	rayDir = rayDir - Vec3<float>(0, 0, 1);
 
 	// Normalize
 	rayDir.normalize();
@@ -125,11 +125,12 @@ void computeRayDir(const int i, const int j, const int imageHeight, const int im
 
 template<typename T>
 Vec3<T> trace(const Vec3<T> &rayorig, const Vec3<T> &raydir, const std::vector<Sphere<T> *> &spheres,
-			  const std::vector<Triangle<T> *> &triangles, const int &depth, const CameraAndColorInfo &cameraAndColorInfo)
+			  const std::vector<Triangle<T> *> &triangles, const int &depth, const CameraAndColorInfo &info)
 {
 	T tnear = INFINITY;
 	const Sphere<T> *sphere = NULL;
-	// find intersection of this ray with the sphere in the scene
+	const Triangle<T> *triangle = NULL;
+	// find intersection of this ray with the spheres in the scene
 	for (unsigned i = 0; i < spheres.size(); ++i) {
 		T t;
 		if (spheres[i]->intersect(rayorig, raydir, &t)) {
@@ -139,54 +140,108 @@ Vec3<T> trace(const Vec3<T> &rayorig, const Vec3<T> &raydir, const std::vector<S
 			}
 		}
 	}
-
-	// if there's no intersection return background color
-	if (!sphere) return cameraAndColorInfo.backgroundColor;
-	Vec3<T> surfaceColor = 0; // color of the ray/surfaceof the object intersected by the ray
-	Vec3<T> phit = rayorig + raydir * tnear; // point of intersection
-	Vec3<T> nhit = phit - sphere->center; // normal at the intersection point
-	nhit.normalize(); // normalize normal direction
-
-
-	// it's a diffuse object, no need to raytrace any further. Shoot shadow rays
-	for (unsigned j = 0; j < spheres.size(); ++j) {
+	// find intersection of this ray with the triangles in the scene
+	for (unsigned i = 0; i < triangles.size(); ++i) {
 		T t;
-		if (spheres[j]->intersect(phit + nhit * bias, lightDirection, &t1)) {
-			transmission = 0;
-			break;
+		if (triangles[i]->intersect(rayorig, raydir, &t)) {
+			if (t < tnear) {
+				tnear = t;
+				triangle = triangles[i];
+				sphere = NULL;
+			}
 		}
 	}
-	surfaceColor += sphere->surfaceColor * transmission * 
-		std::max(T(0), nhit.dot(lightDirection)) * spheres[i]->emissionColor;
 
+	// if there's no intersection return background color
+	if (!sphere && !triangle) return info.backgroundColor;
+	Vec3<T> surfaceColor = 0; // color of the ray/surfaceof the object intersected by the ray
+	Vec3<T> phit = rayorig + raydir * tnear; // point of intersection
+	Vec3<T> nhit;
+	if (sphere)
+		nhit = phit - sphere->center; // normal at the intersection point
+	else if (triangle)
+		nhit = phit + triangle->norm; // normal at the intersection point
+	 
+	nhit.normalize(); // normalize normal direction
+	
+	T bias = 1e-4; // add some bias to the point from which we will be tracing
+	// Shoot shadow rays to see if point is in shadow
+	int notInShadow = 1;
+	for (unsigned i = 0; i < spheres.size(); ++i) {
+		T t;
+		if (spheres[i]->intersect(phit + nhit * bias, info.directionToLight, &t))
+			notInShadow = 0;
+	}
+	for (unsigned i = 0; i < triangles.size(); ++i) {
+		T t;
+		if (triangles[i]->intersect(phit + nhit * bias, info.directionToLight, &t))
+			notInShadow = 0;
+	}
+
+	// Determine intensity information for intersecting point using illumination model
+	Vec3<float> refldir = nhit * 2 * (nhit.dot(info.directionToLight)) - (info.directionToLight);
+	
+	if (sphere)
+	{
+		surfaceColor += sphere->diffuse * info.ambientLight + (info.lightColor
+			* (sphere->diffuse * std::max<float>(0.0, nhit.dot(info.directionToLight))
+			+ sphere->specularHighlight * pow(std::max<float>(0.0, (-raydir).dot(refldir)), sphere->phongConstant)))
+			* notInShadow;
+	}
+	else if (triangle)
+	{
+		surfaceColor += triangle->diffuse * info.ambientLight + (info.lightColor
+			* (triangle->diffuse * std::max<float>(0.0, nhit.dot(info.directionToLight))
+			+ triangle->specularHighlight * pow(std::max<float>(0.0, (-raydir).dot(refldir)), triangle->phongConstant)))
+			* notInShadow;
+	}
+
+	return surfaceColor;
 }
 
 template<typename T>
-void render(const CameraAndColorInfo &camerAndColorInfo, const std::vector<Sphere<T> *> &spheres,
+void render(const CameraAndColorInfo &info, const std::vector<Sphere<T> *> &spheres,
 			const std::vector<Triangle<T> *> &triangles)
 {
-	const unsigned imageHeight = 2;
-	const unsigned imageWidth = 2;
+	const unsigned imageHeight = 513;
+	const unsigned imageWidth = 514;
 	Vec3<T> *image = new Vec3<T>[imageWidth * imageHeight], *pixel = image;
-	float viewWindowValue = 1/cot(camerAndColorInfo.fieldOfView / 2);
+	float viewWindowValue = 1/(1 / tan(info.fieldOfView * (M_PI/180)));
 
-	float height = 0;
-	float width = 0;
+	float height = -.5;
 	// Shoot a ray through every pixel
 	for (int j = 0; j < imageHeight; j++)
 	{
-		height += .5;
-		for (int i = 0; i < imageHeight; i++, pixel++)
+		height += 1;
+		float width = -.5;
+		for (int i = 0; i < imageWidth; i++, pixel++)
 		{
-			width +=.5;
+			width += 1;
 			// Compute primary ray direction
 			Vec3<T> rayDir;
-			computeRayDir<float>(i, j, imageHeight, imageWidth, viewWindowsValue, rayDir);
+			computeRayDir<float>(width, height, imageHeight, imageWidth, viewWindowValue, rayDir);
 
 			// Shoot prim ray into scene and search for intersection
-			*pixel = trace(Vec3<T>(0, 0, 1), raydir, spheres, triangles, 0, camerAndColorInfo);
+			*pixel = trace(info.cameraLookFrom, rayDir, spheres, triangles, 0, info);
 		}
 	}
+
+	// Save result to a PPM image (keep these flags if you compile under Windows)
+	std::ofstream ofs("./diffuse.ppm", std::ios::out | std::ios::binary);
+	ofs << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+	int widthCounter = 0;
+	for (unsigned i = 0; i < imageWidth * imageHeight; ++i) {
+		if (widthCounter == imageWidth - 1) {
+			widthCounter = 0;
+			ofs << '\n';
+		}
+		ofs << (short)(std::min(T(1), image[i].x) * 255) << ' ' << 
+		(short)(std::min(T(1), image[i].y) * 255) << ' ' <<
+		(short)(std::min(T(1), image[i].z) * 255) << ' '; 
+		widthCounter++;
+	}
+	ofs.close();
+	delete [] image;
 }
 
 int main(int argc, char **argv)
--- RayTracer/raytracer.h
+++ RayTracer/raytracer.h
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 
 #define M_PI 3.141592653589793
 #define INFINITY 1e8
@@ -32,9 +34,12 @@ public:
 	Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
 	Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
 	T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }
+	// (a2b3 - a3b2, a3b1 - a1b3, a1b2 - a2b1)
+	Vec3<T> cross(const Vec3<T> &v) const { return Vec3<T>(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
 	Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
 	Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
 	Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
+	Vec3<T>& operator -= (const Vec3<T> &v) { x -= v.x, y -= v.y, z -= v.z; return *this; }
 	Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
 	Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
 	T length2() const { return x * x + y * y + z * z; }
@@ -54,17 +59,17 @@ public:
 	Vec3<T> center;                         /// position of the sphere
 	T radius, radius2;                      /// sphere radius and radius^2
 	Vec3<T> diffuse, specularHighlight, reflective;
-	T phongConstant;
+	T phongConstant, transparency;
 	Sphere() {};
 
 	// compute a ray-sphere intersection using the geometric solution
 	bool intersect(const Vec3<T> &rayorig, const Vec3<T> &raydir, T *t) const
 	{
-		T B = 2 * (raydir.x*rayorig.x - raydir.x*center.x + raydir.y*rayorig.y - raydir.y*center.y - raydir.z*rayorig.z - raydir.z*center.z);
-		T C = pow(rayorig, 2) - 2*rayorig.x*center.x + pow(center.x, 2) + pow(rayorig.y, 2) - 2*rayorig.y*center.y
-			+ pow(center.y, 2) + pow(rayorig.z, 2) - 2*rayorig.z*center.z + pow(center.z, 2);
+		T B = 2 * (raydir.x * rayorig.x - raydir.x * center.x + raydir.y * rayorig.y - raydir.y * center.y + raydir.z * rayorig.z - raydir.z * center.z);
+		T C = pow(rayorig.x, 2) - 2 * rayorig.x * center.x + pow(center.x, 2) + pow(rayorig.y, 2) - 2 * rayorig.y * center.y
+			+ pow(center.y, 2) + pow(rayorig.z, 2) - 2 * rayorig.z * center.z + pow(center.z, 2) - radius2;
 
-		// Compute discriminant D = B^2 -4C
+		// Compute discriminant D = B^2 - 4C
 		T D = pow(B, 2) - 4 * C;
 
 		if (D < 0)
@@ -91,13 +96,118 @@ template<typename T>
 class Triangle 
 {
 public:
-	Vec3<T> p1;
-	Vec3<T> p2;
-	Vec3<T> p3;
+	Vec3<T> p1, p2, p3, norm;
 	Vec3<T> diffuse, specularHighlight;
 	T phongConstant; 
 	Triangle() {};
 
+	// compute a ray-triangle intersection using the geometric solution
+	bool intersect(const Vec3<T> &rayorig, const Vec3<T> &raydir, T *t)
+	{
+		// First compute plane normal
+		Vec3<T> v1 = p1 - p2;
+		Vec3<T> v2 = p3 - p2;
+		Vec3<T> pn = v2.cross(v1).normalize();
+		norm = pn;
+		// compute d
+		T d = -pn.x * p1.x - pn.y * p1.y - pn.z * p1.z;
+
+		// Ray/Plane intersection
+		T vd = pn.dot(raydir);
+		if (vd >= 0) return false;
+		T v0 = -(pn.dot(rayorig) + d);
+		// t = -(ax0 + by0 + cz0 + d) / (axd + byd + czd) 
+		*t = v0 / vd;
+		if (t < 0) return false;
+		if (vd > 0)
+		{
+			pn = -pn;
+			norm = pn;
+		}
+		Vec3<T> r = rayorig + raydir * *t;
+
+		// Drop largest component
+		enum coords { x, y, z };
+		int max = pn.x; /* assume x is the largest */
+		coords dominateCoord = x;
+		if (pn.y > max) { /* if y is larger than max, assign y to max */
+			max = pn.y;
+			dominateCoord = y;
+		} /* end if */
+		if (pn.z > max) { /* if z is larger than max, assign z to max */
+			max = pn.z;
+			dominateCoord = z;
+		} /* end if */
+
+		Vec3<T> projP1;
+		Vec3<T> projP2;
+		Vec3<T> projP3;
+		Vec3<T> projR;
+		switch (dominateCoord) 
+		{
+			case x: 
+				projP1 = Vec3<T>(p1.y, p1.z, 0);
+				projP2 = Vec3<T>(p2.y, p2.z, 0);
+				projP3 = Vec3<T>(p3.y, p3.z, 0);
+				projR = Vec3<T>(r.y, r.z, 0);
+				break;
+			case y: 
+				projP1 = Vec3<T>(p1.x, p1.z, 0);
+				projP2 = Vec3<T>(p2.x, p2.z, 0);
+				projP3 = Vec3<T>(p3.x, p3.z, 0);
+				projR = Vec3<T>(r.x, r.z, 0);
+				break;
+			case z: 
+				projP1 = Vec3<T>(p1.x, p1.y, 0);
+				projP2 = Vec3<T>(p2.x, p2.y, 0);
+				projP3 = Vec3<T>(p3.x, p3.y, 0);
+				projR = Vec3<T>(r.x, r.y, 0);
+				break;
+		}
+
+		projP1 -= projR;
+		projP2 -= projR;
+		projP3 -= projR;
+		projR -= projR;
+
+		std::vector<Vec3<T> *> verts;
+		verts.push_back(&projP1);
+		verts.push_back(&projP2);
+		verts.push_back(&projP2);
+
+		int numCrossings = 0;
+		int signHolder = projP1.x < 0 ? -1 : 1;
+		int n = verts.size();
+		int j = 0;
+		int nextSignHolder;
+		for (int i = 0; i < n; i++)
+		{
+			if (i == n - 1) j = 0;
+			if (verts.at(j + 1)->y < 0)
+				nextSignHolder = -1;
+			else
+				nextSignHolder = 1;
+
+			if ((x < 0) == (y < 0))
+			{
+				if (verts.at(j)->x > 0 && verts.at(j + 1)->x > 0) 
+					numCrossings++;
+				else if (verts.at(j)->x > 0 || verts.at(j + 1)->x > 0)
+				{
+					T ucross = verts.at(j)->x * -verts.at(j)->y
+						* (verts.at(j + 1)->x * -verts.at(j)->x) / (verts.at(j + 1)->y * -verts.at(j)->y);
+					if (ucross > 0) numCrossings++;
+				}
+			}
+			signHolder = nextSignHolder;
+			j++;
+		}
+
+		if (numCrossings % 2 != 0) // is odd
+			return true;
+		else
+			return false;
+	}
 };
 
 // Camera and color info class
